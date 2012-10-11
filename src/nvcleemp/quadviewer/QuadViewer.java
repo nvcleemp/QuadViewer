b9ed636e2b4ea8024b4c8f1d0cc440896c21edee
nvcleemp <nico.vancleemput@gmail.com>
2012-10-11 13:29:49 +0200

    Initial version

---
+++ src/nvcleemp/quadviewer/QuadViewer.java
@@ -0,0 +1,15 @@
+package nvcleemp.quadviewer;
+
+/**
+ *
+ * @author nvcleemp
+ */
+public class QuadViewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
