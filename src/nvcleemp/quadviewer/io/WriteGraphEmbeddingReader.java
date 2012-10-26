c00d7bc23769e05e02ecd4f10da3c0036a2a60c5
nvcleemp <nico.vancleemput@gmail.com>
2012-10-26 14:15:12 +0200

    Fixed some bugs in the WriteGraphEmbeddingReader. Bugs were due to leading and trailing spaces in writegraph files.

--- src/nvcleemp/quadviewer/io/WriteGraphEmbeddingReader.java
+++ src/nvcleemp/quadviewer/io/WriteGraphEmbeddingReader.java
@@ -24,13 +24,13 @@ public class WriteGraphEmbeddingReader {
         for (int i = 0; i < q.getOrder(); i++) {
             if("0".equals(lastRead)) throw new IOException("Reached end of graph to soon.");
             
-            String parts[] = lastRead.split("\\s+");
+            String parts[] = lastRead.trim().split("\\s+");
             embedding[i][0] = Double.parseDouble(parts[1]);
             embedding[i][1] = Double.parseDouble(parts[2]);
             lastRead = br.readLine();
         }
         
-        if(!"0".equals(lastRead))
+        if(!"0".equals(lastRead.trim()))
             throw new IOException("We're finished, but apparently the graph isn't.");
         
         return embedding;
